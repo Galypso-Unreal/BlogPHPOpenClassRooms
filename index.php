7ca406d4f5309d0ed64298be511dcd23402e5dd5
unknown <johann.moser0681@gmail.com>
2023-06-02 16:23:04 +0200

    ADD [page] homepage add with static informations

--- index.php
+++ index.php
@@ -5,4 +5,9 @@ require_once './vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('./src/views');
 $twig = new \Twig\Environment($loader, []);
 
+// define all globals variable usable in twig
+
+$twig->addGlobal('img_assets', 'src/assets/img/');
+
+
 echo $twig->render('index.twig');
