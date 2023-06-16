8bbf75a4b500efa6d9a23033711d7d058ed8bd96
unknown <johann.moser0681@gmail.com>
2023-06-16 16:59:48 +0200

    ADD [post] Adding model for post, database connection, listing on front and end and single article

--- src/lib/database.php
+++ src/lib/database.php
@@ -7,7 +7,7 @@ class DatabaseConnection
 	public function getConnection(): PDO
 	{
     	if ($this->database === null) {
-        	$this->database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'blog', 'password');
+        	$this->database = new PDO('mysql:host=localhost:10070;dbname=blog;charset=utf8', 'root', 'root');
     	}
 
     	return $this->database;
